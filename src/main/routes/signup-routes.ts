45155b9746609c9c57246724d7bafdcb0f23b0d4
lucas.nexatlas <lucas.katayama@planodevoo.net>
2021-05-17 08:58:09 -0300

    feat: add SignUp Factory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,20 +1,6 @@
 import { Router } from 'express'
-import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 export default (router: Router): void => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
-
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
-
-  beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
-    await accountCollection.deleteMany({})
-  })
-  
   router.post('/signup', (req, res) => {
     res.json({ ok: 'ok' })
   })
