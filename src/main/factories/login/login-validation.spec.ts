2583d1642fd61a7db31f1e96854a012be4a84a12
lucas.nexatlas <lucas.katayama@planodevoo.net>
2021-06-16 07:57:27 -0300

    feat: add LoginValidation factory

---
+++ src/main/factories/login/login-validation.spec.ts
@@ -0,0 +1,30 @@
+import { EmailValidation } from "../../../presentation/helper/validators/email-validation";
+import { RequiredFieldValidation } from "../../../presentation/helper/validators/required-field-validation";
+import { Validation } from "../../../presentation/helper/validators/validation";
+import { ValidationComposite } from "../../../presentation/helper/validators/validation-composite";
+import { EmailValidator } from "../../../presentation/protocols/email-validator";
+import { makeLoginValidation } from "./login-validation";
+
+jest.mock('../../../presentation/helper/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+
+describe('LoginValidation Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeLoginValidation()
+    const validations: Validation[] = []
+    for (const field of ['email', 'password']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toBeCalledWith(validations)
+  });
+});
--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,5 +1,4 @@
 export * from '../../protocols'
-export * from '../../protocols/email-validator'
 export * from '../../helper/validators/validation'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
