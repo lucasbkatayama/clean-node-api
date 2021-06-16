336fe57b0ee6ffca75b19867c4f3474698ef2dcf
lucas.nexatlas <lucas.katayama@planodevoo.net>
2021-06-15 21:32:58 -0300

    test: add test to CompareFieldsValidation

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,4 +1,4 @@
-import { CompareFieldValidation } from "../../presentation/helper/validators/compare-fields-validation";
+import { CompareFieldsValidation } from "../../presentation/helper/validators/compare-fields-validation";
 import { EmailValidation } from "../../presentation/helper/validators/email-validation";
 import { RequiredFieldValidation } from "../../presentation/helper/validators/required-field-validation";
 import { Validation } from "../../presentation/helper/validators/validation";
@@ -25,7 +25,7 @@ describe('SignUpValidator Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
     }
-    validations.push(new CompareFieldValidation('password', 'passwordConfirmation'))
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     validations.push(new EmailValidation('email', makeEmailValidator()))
     expect(ValidationComposite).toBeCalledWith(validations)
   });
