a3b0de7a1afdcd0052c2bb67dc99a30fc74d30aa
lucas.nexatlas <lucas.katayama@planodevoo.net>
2021-07-16 08:33:49 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -2,12 +2,12 @@ import env from '../../config/env'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeLoginValidation } from './login-validation-factory'
 import { Controller } from '../../../presentation/protocols'
-import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 import { LoginController } from '../../../presentation/controllers/login/login-controller'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
-import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 
 export const makeLoginController = (): Controller => {
   const salt = 12
